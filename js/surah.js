a6e2d47cb791adef1bfa5a13dbf4b1630006b0e5
whywidodo <mr.wahyuwidodo@gmail.com>
2021-08-02 15:57:34 +0700

    update structure

--- js/surah.js
+++ js/surah.js
@@ -22,7 +22,7 @@ if(datax==1){
                 var ayat_text = detail['data']['verses'][i]['text']['transliteration']['en'];
                 var arti_ayat = detail['data']['verses'][i]['translation']['id'];
                 htA+='<div class="mb-2">';
-                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="font-size:30px;" >'+
+                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="font-size:25px;" >'+
                     ayat +'</div></div>';
                 htA+='<div class="col justify-content-center text-start px-3 mb-2">'+ ayat_text +'</div>';
                 htA+='<div class="col justify-content-center text-start px-3 mb-2"><cite>'+'"'+ arti_ayat + '"' +'</cite></div>';
@@ -45,13 +45,17 @@ if(datax==1){
             var nama_surat = detail['data']['name']['transliteration']['id'];
             var arti_surat = detail['data']['name']['translation']['id'];
             var jumlah_surat = detail['data']['numberOfVerses'];
-            var basmallah = detail['data']['preBismillah']['text']['arab'];
+            if(datax==9){
+                var basmallah = "";
+            }else{
+                var basmallah = detail['data']['preBismillah']['text']['arab'];
+            }
             var jumlah_ayat = detail['data']['verses']['length'];
             
             htTitle+='<div class="mx-2 justify-content-center text-center">'+ nama_surat + ' (' + arti_surat + ')' +'</div>';
 
             htA+='<div class="mb-2">';
-            htA+='<div class="col justify-content-center text-center"><div class="arab" lang="ar" style="line-height:60px;font-size:35px;">'
+            htA+='<div class="col justify-content-center text-center"><div class="arab" lang="ar" style="line-height:60px;font-size:30px;">'
                 + 
                 basmallah
                 +'</div></div>';
@@ -61,7 +65,7 @@ if(datax==1){
                 var ayat_text = detail['data']['verses'][i]['text']['transliteration']['en'];
                 var arti_ayat = detail['data']['verses'][i]['translation']['id'];
                 htA+='<div class="mb-2">';
-                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="line-height:60px;font-size:30px;" >'+ ayat +'</div></div>';
+                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="line-height:60px;font-size:25px;" >'+ ayat +'</div></div>';
                 htA+='<div class="col justify-content-center text-start px-3 mb-2 fw-bold">'+ ayat_text +'</div>';
                 htA+='<div class="col justify-content-center text-start px-3 mb-2"><cite>'+'"'+ arti_ayat + '"' +'</cite></div>';
                 htA+='</div>';
