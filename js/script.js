49a30b9c76c2b74a9091fa5a9c78ed3b09a90420
whywidodo <mr.wahyuwidodo@gmail.com>
2021-08-02 10:09:55 +0700

    update file

--- js/script.js
+++ js/script.js
@@ -13,26 +13,17 @@ $.ajax({
             var jumlah_surat = detail['data'][i]['numberOfVerses'];
             var arabic = detail['data'][i]['name']['short'];
 
-            htA+='<a class="d-block" href="http://localhost/quran/surah.php?id='+surat_ke+'">';
+            htA+='<a class="d-block link-surat" href="http://localhost/quran/surah.html?id='+surat_ke+'">';
             htA+='<div class="card mb-2">';
             htA+='<div class="row px-2" style="min-height: 120px;">';
             htA+='<div class="col-2 justify-content-center align-self-center">'+ surat_ke +'</div>';
             htA+='<div class="col-6 justify-content-center align-self-center"><div class="card-body" >'+ 
                 nama_surat + ' ('+jumlah_surat+')' + '<br/>' + arti_surat
                 +'</div></div>';
-            htA+='<div class="col-4 justify-content-center align-self-center">'+ arabic +'</div>';
+            htA+='<div class="col-4 justify-content-center align-self-center arab">'+ arabic +'</div>';
             htA+='</div>';
             htA+='</div>';
             htA+='</a>';
-
-            
-            // ht+='<div class="media-body">';
-            // ht+='<a class="d-block u-link-v5 g-color-main g-color-primary--hover g-font-weight-600 g-mb-3" href="https://quran.kemenag.go.id/sura/'+data.data[i].id+'">'+data.data[i].surat_name+' ('+data.data[i].count_ayat+')</a>';
-
-            // ht+='<span class="g-font-size-13 g-color-gray-dark-v4 g-mr-15">';
-            //     ht+=data.data[i].surat_terjemahan;
-            //     ht+='</span>';
-            // ht+='</div>';
         }
         for (var i = 38; i < 76; i++) {
             var surat_ke = detail['data'][i]['number'];
@@ -41,15 +32,17 @@ $.ajax({
             var jumlah_surat = detail['data'][i]['numberOfVerses'];
             var arabic = detail['data'][i]['name']['short'];
 
+            htB+='<a class="d-block link-surat" href="http://localhost/quran/surah.html?id='+surat_ke+'">';
             htB+='<div class="card mb-2">';
             htB+='<div class="row px-2" style="min-height: 120px;">';
             htB+='<div class="col-2 justify-content-center align-self-center">'+ surat_ke +'</div>';
             htB+='<div class="col-6 justify-content-center align-self-center"><div class="card-body">'+ 
                 nama_surat + ' ('+jumlah_surat+')' + '<br/>' + arti_surat
                 +'</div></div>';
-            htB+='<div class="col-4 justify-content-center align-self-center">'+ arabic +'</div>';
+            htB+='<div class="col-4 justify-content-center align-self-center arab">'+ arabic +'</div>';
             htB+='</div>';
             htB+='</div>';
+            htB+='</a>';
         }
         for (var i = 76; i < 114; i++) {
             var surat_ke = detail['data'][i]['number'];
@@ -58,16 +51,17 @@ $.ajax({
             var jumlah_surat = detail['data'][i]['numberOfVerses'];
             var arabic = detail['data'][i]['name']['short'];
 
-
+            htC+='<a class="d-block link-surat" href="http://localhost/quran/surah.html?id='+surat_ke+'">';
             htC+='<div class="card mb-2">';
             htC+='<div class="row px-2" style="min-height: 120px;">';
             htC+='<div class="col-2 justify-content-center align-self-center">'+ surat_ke +'</div>';
             htC+='<div class="col-6 justify-content-center align-self-center"><div class="card-body">'+ 
                 nama_surat + ' ('+jumlah_surat+')' + '<br/>' + arti_surat
                 +'</div></div>';
-            htC+='<div class="col-4 justify-content-center align-self-center">'+ arabic +'</div>';
+            htC+='<div class="col-4 justify-content-center align-self-center arab">'+ arabic +'</div>';
             htC+='</div>';
             htC+='</div>';
+            htC+='</a>';
         }
         $(".divA").html(htA);
         $(".divB").html(htB);
--- js/surah.js
+++ js/surah.js
@@ -1,7 +1,5 @@
 let data = new URLSearchParams(window.location.search);
 let datax = data.get("id")
-// let url=  "https://api.quran.sutanlab.id/surah/"+datax;
-// console.log(url);
 if(datax==1){
     $.ajax({
     url: 'https://api.quran.sutanlab.id/surah/'+datax,
@@ -9,26 +7,29 @@ if(datax==1){
     dataType: "json",
     success: function (detail) {
         var htA = "";
+        var htTitle = "";
             var surat_ke = detail['data']['number'];
+            var nama_surat_arab = detail['data']['name']['short'];
             var nama_surat = detail['data']['name']['transliteration']['id'];
             var arti_surat = detail['data']['name']['translation']['id'];
             var jumlah_surat = detail['data']['numberOfVerses'];
             var basmallah = detail['data']['verses'][0]['text']['arab'];
             var jumlah_ayat = detail['data']['verses']['length'];
-            
-            // console.log(jumlah_ayat);
-            htA+='<div class="card mb-2">';
-            htA+='<div class="col justify-content-center text-end"><div class="card-body arab" lang="ar" style="line-height:60px;font-size:30px;">'
-                + 
-                basmallah
-                +'</div></div>';
-            htA+='</div>';
-            for(var i=1; i < jumlah_ayat; i++){
+
+            htTitle+='<div class="mx-2 justify-content-center text-center">'+ nama_surat + ' (' + arti_surat + ')' +'</div>';
+            for(var i=0; i < jumlah_ayat; i++){
                 var ayat = detail['data']['verses'][i]['text']['arab'];
-                htA+='<div class="card mb-2">';
-                htA+='<div class="col justify-content-center text-end"><div class="card-body arab" lang="ar" style="line-height:60px;font-size:30px;" >'+ ayat +'</div></div>';
+                var ayat_text = detail['data']['verses'][i]['text']['transliteration']['en'];
+                var arti_ayat = detail['data']['verses'][i]['translation']['id'];
+                htA+='<div class="mb-2">';
+                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="font-size:30px;" >'+
+                    ayat +'</div></div>';
+                htA+='<div class="col justify-content-center text-start px-3 mb-2">'+ ayat_text +'</div>';
+                htA+='<div class="col justify-content-center text-start px-3 mb-2"><cite>'+'"'+ arti_ayat + '"' +'</cite></div>';
                 htA+='</div>';
+                htA+='<hr class="bg-light border-1 border-top border-dark">';
             }
+        $(".nama-surat").html(htTitle);
         $(".detail-surat").html(htA);
     }
     });
@@ -39,6 +40,7 @@ if(datax==1){
     dataType: "json",
     success: function (detail) {
         var htA = "";
+        var htTitle = "";
             var surat_ke = detail['data']['number'];
             var nama_surat = detail['data']['name']['transliteration']['id'];
             var arti_surat = detail['data']['name']['translation']['id'];
@@ -46,18 +48,26 @@ if(datax==1){
             var basmallah = detail['data']['preBismillah']['text']['arab'];
             var jumlah_ayat = detail['data']['verses']['length'];
             
-            htA+='<div class="card mb-2">';
-            htA+='<div class="col justify-content-center text-end"><div class="card-body arab" lang="ar" style="line-height:60px;font-size:30px;">'
+            htTitle+='<div class="mx-2 justify-content-center text-center">'+ nama_surat + ' (' + arti_surat + ')' +'</div>';
+
+            htA+='<div class="mb-2">';
+            htA+='<div class="col justify-content-center text-center"><div class="arab" lang="ar" style="line-height:60px;font-size:35px;">'
                 + 
                 basmallah
                 +'</div></div>';
             htA+='</div>';
             for(var i=0; i < jumlah_ayat; i++){
                 var ayat = detail['data']['verses'][i]['text']['arab'];
-                htA+='<div class="card mb-2">';
-                htA+='<div class="col justify-content-center text-end"><div class="card-body arab" lang="ar" style="line-height:60px;font-size:30px;" >'+ ayat +'</div></div>';
+                var ayat_text = detail['data']['verses'][i]['text']['transliteration']['en'];
+                var arti_ayat = detail['data']['verses'][i]['translation']['id'];
+                htA+='<div class="mb-2">';
+                htA+='<div class="col justify-content-center text-end"><div class="arab" lang="ar" style="line-height:60px;font-size:30px;" >'+ ayat +'</div></div>';
+                htA+='<div class="col justify-content-center text-start px-3 mb-2 fw-bold">'+ ayat_text +'</div>';
+                htA+='<div class="col justify-content-center text-start px-3 mb-2"><cite>'+'"'+ arti_ayat + '"' +'</cite></div>';
                 htA+='</div>';
+                htA+='<hr class="bg-light border-1 border-top border-dark">';
             }
+        $(".nama-surat").html(htTitle);
         $(".detail-surat").html(htA);
     }
     });
